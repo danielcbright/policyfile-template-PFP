4a9e703dea6c32e1418f7f3ccd84b5331f0cfda7
Daniel Bright <danielcbright@gmail.com>
2020-07-09 18:34:47 -0400

    initial commit
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

--- Policyfile.rb
+++ Policyfile.rb
@@ -4,15 +4,13 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name 'linux-base'
+name 'POLICYNAME'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'linux-base::default'
+run_list 'POLICYNAME::default'
 
-# Specify a custom source for a single cookbook:
-cookbook 'os-hardening', github: 'dev-sec/chef-os-hardening', branch: 'master'
-cookbook 'selinux', github: 'chef-cookbooks/selinux', branch: 'master'
-cookbook 'linux-base', path: '.'
+# Specify Base include_policy:
+include_policy 'base-linux', policy_name: 'base-linux', policy_group: 'unstable', server: 'https://automate.dbright.io/organizations/dbright'
